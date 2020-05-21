dd01a729605fb1b060c1706af2b47dbd7cdca7c8
Vikram Rajan <vikramrajan90@gmail.com>
2020-05-21 14:38:43 -0700

    I don't remember what I changed

--- scraper.py
+++ scraper.py
@@ -280,7 +280,7 @@ def is_valid(url):
             return not re.match(
                 r".*\.(css|js|bmp|gif|jpe?g|ico"
                 + r"|png|tiff?|mid|mp2|mp3|mp4"
-                + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+                + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf|mpg"
                 + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
                 + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
                 + r"|epub|dll|cnf|tgz|sha1"
